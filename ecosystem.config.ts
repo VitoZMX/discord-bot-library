3ddfaa21b1d4262b4e30184c54a38316bfe46cd2
Victor <Vi-69@yandex.ru>
2025-01-04 12:57:09 +0300

    refactor: change name process

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,7 +1,7 @@
 const config = {
   apps: [
     {
-      name: "ds_caretaker",
+      name: "DIS_caretaker",
       script: "./build/src/bots/zmx_caretaker_bot/zmx_caretaker_bot_v1.js",
       watch: ["./build/src"],
       ignore_watch: ["node_modules"],
